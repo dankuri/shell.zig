ecdfdad67bea4c670dd1225e47c60a764cdb531f
dankuri <danek.il420@gmail.com>
2024-12-27 00:52:30 +0500

    repl

--- src/main.zig
+++ src/main.zig
@@ -2,11 +2,14 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
 
-    const stdin = std.io.getStdIn().reader();
-    var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+    while (true) {
+        try stdout.print("$ ", .{});
 
-    try stdout.print("{s}: command not found\n", .{user_input});
+        const stdin = std.io.getStdIn().reader();
+        var buffer: [1024]u8 = undefined;
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+
+        try stdout.print("{s}: command not found\n", .{user_input});
+    }
 }
