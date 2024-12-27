e15a1c1446bbf0ba2ab0c535585d55f3118a8b8f
dankuri <danek.il420@gmail.com>
2024-12-28 00:10:11 +0500

    parse args with single quote

--- src/main.zig
+++ src/main.zig
@@ -22,68 +22,115 @@ pub fn main() !void {
             return stdout.print("\n", .{});
         };
 
-        var arg_iter = std.mem.tokenizeScalar(u8, user_input, ' ');
-        const command = arg_iter.next();
-
-        if (command) |c| {
-            if (std.mem.eql(u8, c, "exit")) {
-                const exit_code = try std.fmt.parseInt(u8, arg_iter.next() orelse "0", 10);
-                std.process.exit(exit_code);
-            } else if (std.mem.eql(u8, c, "echo")) {
-                while (arg_iter.next()) |arg| {
-                    try stdout.print("{s} ", .{arg});
-                }
-                try stdout.print("\n", .{});
-            } else if (std.mem.eql(u8, c, "type")) {
-                try handle_type(allocator, &arg_iter);
-            } else if (std.mem.eql(u8, c, "pwd")) {
-                const pwd = try std.process.getCwdAlloc(allocator);
-                defer allocator.free(pwd);
-                try stdout.print("{s}\n", .{pwd});
-            } else if (std.mem.eql(u8, c, "cd")) {
-                var path = arg_iter.next() orelse "~";
-
-                // this is a bit better than what is in the task, so it behaves more like a real shell
-                if (path[0] == '~') {
-                    const home = std.posix.getenv("HOME") orelse "";
-                    var list = try std.ArrayList(u8).initCapacity(allocator, home.len + path.len - 1);
-                    defer list.deinit();
-                    try list.appendSlice(home);
-                    try list.appendSlice(path[1..]);
-                    path = list.items;
-                    std.process.changeCurDir(path) catch {
-                        try stdout.print("cd: {s}: No such file or directory\n", .{path});
-                    };
-                } else {
-                    std.process.changeCurDir(path) catch {
-                        try stdout.print("cd: {s}: No such file or directory\n", .{path});
-                    };
-                }
+        var args = parse_args(allocator, user_input) catch |err| {
+            if (err == error.UnclosedSingleQuote) {
+                try stdout.print("error: unclosed single quote\n", .{});
             } else {
-                if (find_exec(allocator, c)) |full_path| {
-                    defer allocator.free(full_path);
+                try stdout.print("error: {s}\n", .{@errorName(err)});
+            }
+            continue;
+        };
 
-                    var argv = std.ArrayList([]const u8).init(allocator);
-                    defer argv.deinit();
+        if (args.len == 0) {
+            continue;
+        }
 
-                    try argv.append(full_path);
+        const command = args[0];
+        args = args[1..];
 
-                    while (arg_iter.next()) |arg| {
-                        try argv.append(arg);
-                    }
+        if (std.mem.eql(u8, command, "exit")) {
+            const exit_code = if (args.len != 0)
+                try std.fmt.parseInt(u8, args[0], 10)
+            else
+                0;
+            std.process.exit(exit_code);
+        } else if (std.mem.eql(u8, command, "echo")) {
+            for (args) |arg| {
+                try stdout.print("{s} ", .{arg});
+            }
+            try stdout.print("\n", .{});
+        } else if (std.mem.eql(u8, command, "type")) {
+            try handle_type(allocator, args);
+        } else if (std.mem.eql(u8, command, "pwd")) {
+            const pwd = try std.process.getCwdAlloc(allocator);
+            defer allocator.free(pwd);
+            try stdout.print("{s}\n", .{pwd});
+        } else if (std.mem.eql(u8, command, "cd")) {
+            var path = if (args.len != 0) args[0] else "~";
 
-                    var child = std.process.Child.init(argv.items, allocator);
-                    _ = try child.spawnAndWait();
-                } else {
-                    try stdout.print("{s}: command not found\n", .{c});
+            // this is a bit better than what is in the task, so it behaves more like a real shell
+            if (path[0] == '~') {
+                const home = std.posix.getenv("HOME") orelse "";
+                var list = try std.ArrayList(u8).initCapacity(allocator, home.len + path.len - 1);
+                defer list.deinit();
+                try list.appendSlice(home);
+                try list.appendSlice(path[1..]);
+                path = list.items;
+                std.process.changeCurDir(path) catch {
+                    try stdout.print("cd: {s}: No such file or directory\n", .{path});
+                };
+            } else {
+                std.process.changeCurDir(path) catch {
+                    try stdout.print("cd: {s}: No such file or directory\n", .{path});
+                };
+            }
+        } else {
+            if (find_exec(allocator, command)) |full_path| {
+                defer allocator.free(full_path);
+
+                var argv = std.ArrayList([]const u8).init(allocator);
+                defer argv.deinit();
+
+                try argv.append(full_path);
+
+                for (args) |arg| {
+                    try argv.append(arg);
                 }
+
+                var child = std.process.Child.init(argv.items, allocator);
+                _ = try child.spawnAndWait();
+            } else {
+                try stdout.print("{s}: command not found\n", .{command});
             }
         }
     }
 }
 
-fn handle_type(allocator: std.mem.Allocator, arg_iter: *std.mem.TokenIterator(u8, .scalar)) !void {
-    while (arg_iter.next()) |arg| {
+fn parse_args(allocator: std.mem.Allocator, input: []u8) ![][]u8 {
+    var args_list = std.ArrayList([]u8).init(allocator);
+    var in_single_quote = false;
+    var arg_builder = std.ArrayList(u8).init(allocator);
+    for (input) |char| {
+        if (in_single_quote) {
+            if (char == '\'') {
+                in_single_quote = false;
+                try args_list.append(try arg_builder.toOwnedSlice());
+                continue;
+            }
+            try arg_builder.append(char);
+        } else {
+            if (char == '\'') {
+                in_single_quote = true;
+            } else if (char == ' ' and arg_builder.items.len != 0) {
+                try args_list.append(try arg_builder.toOwnedSlice());
+            } else if (char != ' ') {
+                try arg_builder.append(char);
+            }
+        }
+    }
+    if (in_single_quote) {
+        return error.UnclosedSingleQuote;
+    }
+
+    if (arg_builder.items.len != 0) {
+        try args_list.append(try arg_builder.toOwnedSlice());
+    }
+
+    return args_list.toOwnedSlice();
+}
+
+fn handle_type(allocator: std.mem.Allocator, args: [][]u8) !void {
+    for (args) |arg| {
         if (std.meta.stringToEnum(Command, arg) != null) {
             try stdout.print("{s} is a shell builtin\n", .{arg});
         } else {
